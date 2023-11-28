fd117a6c2dbd422471a2d39195964b3c89ae2042
rashrahman <90875950+rashrahman@users.noreply.github.com>
2023-11-28 16:57:23 -0500

    removing files

--- client/firststep/src/App.js
+++ client/firststep/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Registration from "./pages/Registration";
+
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path ="/registration" element={<Registration />} />
+          
         </Routes>
       </div>
     </Router>
