2662f80161d1a4f61a7a84514844d49f67a4f923
Rashik <rashikhussain@gmail.com>
2023-12-01 23:28:49 -0500

    Login Page made

--- client/firststep/src/components/LoginForm.js
+++ client/firststep/src/components/LoginForm.js
@@ -1,1 +1,42 @@
-//created login form
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you would handle the login logic, perhaps sending a POST request to your server
+        console.log(username, password);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username or Email:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    required
+                />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default LoginForm;
---
+++ client/firststep/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+// LoginPage.js
+import React from 'react';
+import LoginForm from '../components/LoginForm'; 
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h2>Login to Your Account</h2>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
