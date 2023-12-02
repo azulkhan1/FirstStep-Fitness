5fbbc18a40f4bb8b40dfcd85316c77a061debe86
Rashik <rashikhussain@gmail.com>
2023-12-02 00:51:55 -0500

    login page finalized

--- client/firststep/src/App.js
+++ client/firststep/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Workout from "./pages/Workout";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/workout" element={<Workout />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
     </Router>
--- client/firststep/src/components/LoginForm.js
+++ client/firststep/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
