1c940eee903719f2ad14b45b01b3ce193b384806
Zuizz Saeed <zuizzms@bu.edu>
2023-12-03 00:37:38 -0500

    p

---
+++ client/firststep/src/pages/GetBMI.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function GetBMI () {
+
+    return (
+       <div>
+         
+          <div>
+
+            <h1> GetBMI</h1>
+            <p> Make the first step to a healthier life </p>
+          </div>
+        
+          
+        </div>
+      ); 
+    }
+    
+    export default GetBMI
