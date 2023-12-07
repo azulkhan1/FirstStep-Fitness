a6969c7069f2445954cab39822ca612f693cd28f
rashrahman <90875950+rashrahman@users.noreply.github.com>
2023-12-07 17:29:47 -0500

    about page

--- client/firststep/src/pages/About.js
+++ client/firststep/src/pages/About.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import "../styles/About.css";
+import background from "../images/about.jpg"
 const AboutUs = () => {
   return (
-    <div>
+    <div className = "about">
+      <div className = "aboutTop" 
+       style ={{ backgroundImage: `url(${background})`}}>
+       </div>
+      <div className = "aboutBottom">
       <h1>About Us</h1>
-      <p>This is the About Us page content.</p>
+      <p>Welcome to FirstStep Fitness - Your Personal Health Companion!
+      
+         Here, you can use our web application to do fun things health related such as calculate your BMI, or get 
+        exciting, randomized workout plans to spice up your daily routine. 
+      
+      
+      At the core of our service is a simple yet powerful BMI calculator. We understand that health is not one-size-fits-all, and that's
+       why our BMI calculator takes into account your unique height and weight to provide you with an accurate measure of your body mass index. 
+       It's a crucial step towards understanding your body and taking informed steps towards a healthier lifestyle.
+      
+      But we don't stop there. FirstStep Fitness goes beyond mere calculations. We're here to inspire and guide you on your fitness journey. 
+      Our platform generates personalized, and randomized workout plans tailored to your preferences, and goals. 
+      </p>
+      </div>
     </div>
   );
 };
