f3a8b1a8837afa64b2b9aec4f61b17f712fca0d0
azulkhan1 <azulkhan13@gmail.com>
2023-12-07 19:42:02 -0500

    finish bmi page

--- client/firststep/src/pages/GetBMI.js
+++ client/firststep/src/pages/GetBMI.js
@@ -1,18 +1,52 @@
-import React from 'react';
-function GetBMI () {
+import React, { useState } from "react";
 
-    return (
-       <div>
-         
-          <div>
+function GetBMI() {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState(null);
 
-            <h1> GetBMI</h1>
-            <p> Make the first step to a healthier life </p>
-          </div>
-        
-          
-        </div>
-      ); 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3001/bmi", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ height, weight }),
+      });
+      const result = await response.json();
+      const data = JSON.parse(result.getBmi);
+      console.log(data);
+      setBmi(data.bmi);
+      console.log(bmi);
+    } catch (error) {
+      console.error("Error:", error);
     }
-    
-    export default GetBMI
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Height (cm):
+          <input
+            type="number"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </label>
+        <label>
+          Pounds (lb):
+          <input
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </label>
+        <button type="submit">Calculate BMI</button>
+      </form>
+      <p>Your BMI is: {bmi}</p>
+    </div>
+  );
+}
+
+export default GetBMI;
